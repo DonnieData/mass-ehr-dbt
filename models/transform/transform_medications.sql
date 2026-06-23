bfbb0dc0a1226adc833e8ebad58d653c38a608bd
DonnieData <donnie.data@gmail.com>
2026-06-23 00:14:30 +0000

    adding claims model

--- models/transform/transform_medications.sql
+++ models/transform/transform_medications.sql
@@ -1,28 +1,31 @@
--- --select transform_procedures
+-- --select transform_medications
 
 {{ config(
     materialized='incremental',
     schema='massachusetts_ehr',
-    alias='procedures',
+    alias='medications',
     incremental_strategy='append',
     on_schema_change='append_new_columns'
 ) }}
 
 select 
+
     "START"::TIMESTAMP_NTZ AS "START",
     "STOP"::TIMESTAMP_NTZ AS "STOP",
     PATIENT ,
+    PAYER ,
     ENCOUNTER ,
-    SYSTEM ,
     CODE ,
     DESCRIPTION ,
-    BASE_COST::NUMBER(10, 2) AS BASE_COST,
+    BASE_COST::NUMBER(10, 2) AS BASE_COST ,
+    PAYER_COVERAGE::NUMBER(10, 2) AS PAYER_COVERAGE,
+    DISPENSES INT,
+    TOTALCOST::NUMBER(12, 2) AS TOTALCOST,
     REASONCODE ,
-    REASONDESCRIPTION,
+    REASONDESCRIPTION ,
     'MASS-EHR'::VARCHAR as PIPELINE_NAME,
     '{{ invocation_id }}'::VARCHAR AS DBT_RUN_ID ,
     '{{ model.name }}'::VARCHAR AS DBT_MODEL_NAME,
     CURRENT_TIMESTAMP()::TIMESTAMP_NTZ AS LOAD_TS 
    
-from {{ source('massachusetts_ehr', 'PROCEDURES') }}
-
+from {{ source('massachusetts_ehr', 'MEDICATIONS') }}
